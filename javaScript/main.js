f8fa17f97c49eee367179b4ea3801bca2f3e5b8c
AhiShahar <ahishahar1987@gmail.com>
2016-11-07 09:44:49 +1100

    writing the logic on a bus ride can only end well

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,23 +1,25 @@
-console.log("Hello Tic Tac Hoe");
+
 
 var $gameTiles = $(".board div div");
-
-console.log($gameTiles);
-
+var $mark = "";
 var counter = 0;
 var markTile = function() {
   if (counter%2 === 0){
     if ($(this).html() === "") {
-      $(this).html("X");
-      return counter++;
+      $mark = "X";
+      $(this).html($mark);
+      // counter++;
+      checkWin();
     } else {
       alert("tile is already taken, please choose another tile");
       markTile();
     }
   } else {
     if ($(this).html() === "") {
-      $(this).html("O");
-      return counter++;
+      $mark = "O";
+      $(this).html($mark);
+      // counter++;
+      checkWin();
     } else {
       alert("tile is already taken, please choose another tile");
       markTile();
@@ -26,3 +28,39 @@ var markTile = function() {
 };
 
 $gameTiles.on("click", markTile);
+
+var checkWin = function() {
+  // check the top right cube, then check all the lines coming out of it
+  if ($(".cube1").html() !== "") {
+    if ($(".cube1").html() === $(".cube2").html() && $(".cube1").html() === $(".cube3").html()) {
+      console.log($mark + " top row");
+    } else if ($(".cube1").html() === $(".cube4").html() && $(".cube1").html() === $(".cube7").html()) {
+      console.log($mark + " left column");
+    } else if ($(".cube1").html() === $(".cube5").html() && $(".cube1").html() === $(".cube9").html()) {
+      console.log($mark + " left diagonal");
+    }
+  }
+  if ($(".cube4").html() !== "") {
+    if ($(".cube4").html() === $(".cube5").html() && $(".cube4").html() === $(".cube6").html()) {
+      console.log($mark + " mid row");
+    }
+  }
+  if ($(".cube2").html() !== "") {
+    if ($(".cube2").html() === $(".cube5").html() && $(".cube2").html() === $(".cube8").html()) {
+      console.log($mark + " mid column");
+    }
+  }
+  if ($(".cube3").html() !== "") {
+    if ($(".cube3").html() === $(".cube6").html() && $(".cube3").html() === $(".cube9").html()) {
+      console.log($mark + " right column");
+    }
+  }
+  if ($(".cube7").html() !== "") {
+    if ($(".cube7").html() === $(".cube8").html() && $(".cube7").html() === $(".cube9").html()) {
+      console.log($mark + " bottom row");
+    } else if ($(".cube7").html() === $(".cube5").html() && $(".cube7").html() === $(".cube3").html()) {
+      console.log($mark + " right diagonal");
+    }
+  }
+
+};
