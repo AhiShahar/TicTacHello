6b084003471b8ef78257e2b93a1223a540128163
AhiShahar <ahishahar1987@gmail.com>
2016-11-06 15:59:49 +1100

    added the board, the x and o markers, and the no double up function

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,1 +1,28 @@
 console.log("Hello Tic Tac Hoe");
+
+var $gameTiles = $(".board div div");
+
+console.log($gameTiles);
+
+var counter = 0;
+var markTile = function() {
+  if (counter%2 === 0){
+    if ($(this).html() === "") {
+      $(this).html("X");
+      return counter++;
+    } else {
+      alert("tile is already taken, please choose another tile");
+      markTile();
+    }
+  } else {
+    if ($(this).html() === "") {
+      $(this).html("O");
+      return counter++;
+    } else {
+      alert("tile is already taken, please choose another tile");
+      markTile();
+    }
+  }
+};
+
+$gameTiles.on("click", markTile);
